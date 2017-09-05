e9c6b4b1e579da14eab142982c5443f518ef78c3
yan <yan@test.com>
2017-09-05 18:23:52 +0800

    change to Idea

---
+++ src/main/java/com/ampm365/test/serverapi/member/Verfication/TestVerfication.java
@@ -0,0 +1,22 @@
+package com.ampm365.test.serverapi.member.Verfication;
+
+import com.ampm365.test.serverapi.enumerations.Enumerations;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.ibatis.logging.log4j.Log4jImpl;
+import org.testng.Assert;
+
+public class TestVerfication {
+    private static Log4jImpl logger = new Log4jImpl(TestVerfication.class.getName());
+    private static String ResponseContent;
+
+    public static void VerfyResponseStatus(CloseableHttpResponse response,Enumerations.HttpResponseCode expectedStatusCode){
+        if(response == null){
+            logger.debug("Response is NULL");
+            return;
+        }
+        else{
+            int ActualStatusCode = response.getStatusLine().getStatusCode();
+            Assert.assertEquals(ActualStatusCode,expectedStatusCode.value(),String.format("HTTP Status Code is",ActualStatusCode));
+        }
+    }
+}

b7b1fe9443307242ec9b74a5074fb73a3e77f030
yan <yan@test.com>
2017-12-27 18:34:49 +0800

    update api test

---
+++ src/main/java/com/ampm365/test/serverapi/utility/HttpUtility.java
@@ -0,0 +1,50 @@
+package com.ampm365.test.serverapi.utility;
+
+import com.alibaba.fastjson.JSON;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.ibatis.logging.log4j.Log4jImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpUtility {
+    public Log4jImpl logger = new Log4jImpl(HttpUtility.class.getName());
+
+    public static HttpPost SetPostRequestHeadersByJsonString(HttpPost Request, String jsonString){
+        Map<String, String> jsonMap = (Map)JSON.parse(jsonString);
+        for (Map.Entry<String, String> item : jsonMap.entrySet()){
+            Request.addHeader(item.getKey(), item.getValue());
+        }
+        return Request;
+    }
+
+    public static HttpPost SetPostRequestHeaderByMap(HttpPost Request, Map<String, String> headerMap){
+        return Request;
+    }
+
+    public static HttpPost SetPostRequestHeader(HttpPost Request, String HeaderName, String Value){
+        Request.setHeader(HeaderName,Value);
+        return Request;
+    }
+
+
+//    String jsonString = "{\n" + "\"platform\": \"1\",\n" + "\"device\": \"MEIZU-MX3\",\n" + "\"networkType\": \"4G\",\n" + "\"smartLoading\": \"1\",\n" + "\"screen\": \"1920*1080\",\n" + "\"uuid\": \"sadfsadfasdf\",\n" + "\"currentTime\": \"12345\",\n" + "\"installTime\": \"12345\",\n" + "\"channelId\": \"appstore\",\n" + "\"version\": \"1.0.0\",\n" + "\"apiVersion\": \"1.0.0\",\n" + "\"storeId\": \"10001000100\",\n" + "\"vendorId\": \"10001\"\n" + "}";
+//    Map<String, String> jsonMap = (Map) JSON.parse(jsonString);
+////	  logger.debug(jsonMap.toString());
+//          for (Map.Entry<String, String> item : jsonMap.entrySet()) {
+//        postRequest.addHeader(item.getKey(), item.getValue());
+//    }
+//    List<NameValuePair> list = new ArrayList<NameValuePair>();
+//          list.add(new BasicNameValuePair("phoneNumber", TestDataGenerator.generatorMobile()));
+//          list.add(new BasicNameValuePair("phoneCode", "123456"));
+//          list.add(new BasicNameValuePair("password", "123456"));
+//    UrlEncodedFormEntity uefEntity = new UrlEncodedFormEntity(list, "UTF-8");
+//          postRequest.setEntity(uefEntity);
+
+
+}

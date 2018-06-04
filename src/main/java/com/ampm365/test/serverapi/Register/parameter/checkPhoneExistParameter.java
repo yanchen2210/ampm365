cf1da3ca1c6cadcf93f1a87c6e423650683e888c
yan <yan@test.com>
2018-06-04 10:52:48 +0800

    update

---
+++ src/main/java/com/ampm365/test/serverapi/Register/parameter/checkPhoneExistParameter.java
@@ -0,0 +1,8 @@
+package com.ampm365.test.serverapi.Register.parameter;
+
+import com.ampm365.test.serverapi.constants.Constants;
+
+public class checkPhoneExistParameter extends RegisterLoginParameter {
+    private String RequestUrl = Constants.CheckPhoneExist;
+
+}
